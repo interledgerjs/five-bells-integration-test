476839908bf23405b81d0ebedfb40dd29f1bcf3b
sentientwaffle <sentientwaffle@gmail.com>
2016-10-19 11:24:45 -0700

    fix: use correct auth for sendRoutes

--- src/tests/advanced.js
+++ src/tests/advanced.js
@@ -358,14 +358,19 @@ describe('Advanced', function () {
     })
 
     it('routes payments to unknown ledgers to nearby connectors', function * () {
-      yield services.sendRoutes('test2.group1.ledger2.', 'michelle2', [{
+      yield services.sendRoutes([{
         source_ledger: 'test2.group1.ledger2.',
         destination_ledger: 'test2.group2.',
         min_message_window: 5,
         source_account: 'test2.group1.ledger2.milo2',
         // This curve is only used for route selection, not for quoting amounts.
         points: [ [0, 0], [1000, 2000] ]
-      }])
+      }], {
+        ledger: 'test2.group1.ledger2.',
+        connectorName:  'michelle2',
+        username: 'milo2',
+        password: 'milo2'
+      })
       yield services.sendPayment({
         sourceAccount: 'test2.group1.ledger1.alice',
         sourcePassword: 'alice',
